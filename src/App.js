36e9fc53e7dc27cca7df9aff90b251b90c217759
HiranthaHettiarachchi <hirantha.hettiarachchi@bentley.com>
2024-02-13 08:29:56 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Infrastructure Insights
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
